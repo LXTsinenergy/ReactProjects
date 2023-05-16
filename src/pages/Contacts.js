a6258b79de09ca458b758f7fa00c60efca6176d5
LXTsinenergy <bellerstas3002@gmail.com>
2023-05-16 21:00:50 +0300

    first commit

---
+++ src/pages/Contacts.js
@@ -0,0 +1,32 @@
+const Contacts = () => {
+  return (
+    <main className="section">
+      <div className="container">
+        <h1 className="title-1">Contacts</h1>
+
+        <ul className="content-list">
+          <li className="content-list__item">
+            <h2 className="title-2">Location</h2>
+            <p>Bugulma, Russia</p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Telegram / WhatsApp</h2>
+            <p>
+              <a href="tel:+79051234567">+7 (927) 494-75-27</a>
+            </p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Email</h2>
+            <p>
+              <a href="mailto:webdev@protonmail.com">
+                bellerstas3002@gmail.com
+              </a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default Contacts;
